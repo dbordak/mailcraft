7401a285ce6357729fe5e32149e3e5e35e12249c
Revan Sopher <rsopher@gmail.com>
2013-10-13 02:54:26 -0400

    Scrape threads from context.io, build array of arrays where each element is metadata on a message in a thread.

---
+++ getmail.py
@@ -0,0 +1,39 @@
+import contextio as c
+from secrets import CONSUMER_KEY, CONSUMER_SECRET
+
+def getmail():
+    context_io = c.ContextIO(
+        consumer_key=CONSUMER_KEY, 
+        consumer_secret=CONSUMER_SECRET,
+        #debug=True
+    )
+
+    accounts = context_io.get_accounts(email='rsopher@gmail.com')
+
+    data=[]
+
+    threads = accounts[0].get_threads()
+
+    for thread in threads:
+        thread.get()
+        threaddata=[]
+        if thread.messages:
+            for mes in thread.messages:
+                try:
+                    threaddata.append( { 'subject': mes.subject,
+                        'date': mes.date,
+                        'from': mes.addresses['from']['email'],
+                        'numrecip': len(mes.addresses['to'])+len(mes.addresses['cc'])
+                    })
+                except:
+                    try:
+                        threaddata.append( { 'subject': mes.subject,
+                        'date': mes.date,
+                        'from': mes.addresses['from']['email'],
+                        'numrecip': len(mes.addresses['to'])
+                    })
+                    except:
+                        0
+        data.append(threaddata)
+
+    return data
